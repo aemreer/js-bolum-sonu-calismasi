685397f5c81ccc54547b79ae5cd38caee0c289f7
Ahmet Emre Er <emre0720er@hotmail.com>
2023-07-01 22:09:28 +0300

    js-bolum-sonu-calismasi
    Javascript bölüm sonu çalışması

---
+++ script.js
@@ -0,0 +1,71 @@
+// Kosullarla Calismak Bolum Sonu Egzersizi:
+
+/* 
+1- prompt ile aldigin bilgiye gore asagidaki yapiyi kullanarak notun bilgisini #info'ya yazdir
+AA  90-100
+BA  85-89
+BB  80-84
+CB  75-79
+CC  70-74
+DC  65-69
+DD  60-64
+FD  50-59
+FF  0-49
+1
+2- kullanicinin girdigi verinin istedigimiz aralikta olup olmadigini kontrol et
+3- ff bilgisinde uzgun surat ikonu cikart, digerlerinde ise gulucuk olsun :)
+4- ff class bilgisi text-danger, digerlerinin ise text-primary olsun
+*/
+
+const smile= `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zM4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+</svg>
+`
+const frown= `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm-2.715 5.933a.5.5 0 0 1-.183-.683A4.498 4.498 0 0 1 8 9.5a4.5 4.5 0 0 1 3.898 2.25.5.5 0 0 1-.866.5A3.498 3.498 0 0 0 8 10.5a3.498 3.498 0 0 0-3.032 1.75.5.5 0 0 1-.683.183zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+</svg>
+`
+
+
+let puan = prompt("Lütfen Notunuzu Girin!","Örnek: 90")
+let info = document.querySelector(".info")
+
+if (puan>=0 && puan<=100) {
+      info.classList.add("text-primary")  
+      info.innerHTML =`Puanınız : ${puan} ${smile}`
+    if (puan>=90) {
+        info.innerHTML+="AA"
+    }
+    else if (puan   >=  85) {
+        info.innerHTML+="BA"
+    }
+    else if (puan>=80 ) {
+        info.innerHTML+="BB"
+    }
+    else if (puan>=75) {
+        info.innerHTML+="CB"
+    }
+    else if (puan>=70) {
+        info.innerHTML+="CC"
+    }
+    else if (puan>=65) {
+        info.innerHTML+="DC"
+    }
+    else if (puan>=60) {
+        info.innerHTML+="DD"
+    }
+    else if (puan>=50) {
+        info.innerHTML+="FD"
+    }
+    else if (puan<=49) {
+        info.innerHTML=`Puanınız :${puan}  ${frown} FF`
+        info.classList.add("text-danger")
+    }
+
+}
+
+else{
+    info.innerHTML="Bilgiler doğru değil!"
+}
